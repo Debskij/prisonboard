f9c346140c292d3282746ce597dc14b7f1d871f7
BazyliP <bazyli.polednia@gmail.com>
2021-11-21 17:06:21 +0100

    Search for prisoner qualifications

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,3 +1,3 @@
 from .index import index
 from .login import login_get
-from .qualifications import get_qualifications_no_selections, get_qualifications
+from .qualifications import get_qualifications_no_selections, get_qualifications, post_qualifications
