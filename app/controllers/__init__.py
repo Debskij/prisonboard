c53a554a5bc879224028aec84dfa2bb0811f7459
BazyliP <bazyli.polednia@gmail.com>
2021-11-26 00:29:01 +0100

    List prisoners and searching almost done

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -6,3 +6,10 @@ from .qualifications import (
     post_qualifications,
     update_qualifications,
 )
+from .prisoners import (
+    get_all_prisoners,
+    get_prisoners_by_name,
+    get_prisoners_by_surname,
+    get_prisoners_by_position,
+    get_prisoners_by_field_redirect,
+)
