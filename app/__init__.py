058346eb0938f454ddea5b32c096efb6fb59bfb4
BazyliP <bazyli.polednia@gmail.com>
2021-11-26 01:04:44 +0100

    Add Company model

--- app/__init__.py
+++ app/__init__.py
@@ -41,13 +41,21 @@ def init_db():
     db.session.add(
         Prisoner(105, "Maria", "Daciuk", "81235601223", 4.2, True, datetime(1996, 2, 1))
     )
+    
     db.session.add(SystemUser(200, "Adam", "Nowak", "adamos", "123456"))
+    
     db.session.add(Qualification(100, "Plumbering", 3))
     db.session.add(Qualification(100, "CNC machinery", 4))
-    db.session.add(JobOffer(300, "Plumber", 15.5, 30))
-    db.session.add(JobOffer(301, "Tech guy", 15.5, 30))
-    db.session.add(JobOffer(302, "Lecturer", 15.5, 30))
+    
+    db.session.add(Company(200, "ul. Jakaśtam 24, Warszawa", "Tiropolex sp. z o. o.", "Tiropolex"))
+    db.session.add(Company(201, "ul. Zmyślona 14, Warszawa", "Google Ltd.", "Google"))
+    
+    db.session.add(JobOffer(300, 200, "Plumber", 15.5, 30))
+    db.session.add(JobOffer(301, 201, "Tech guy", 15.5, 30))
+    db.session.add(JobOffer(302, 201, "Lecturer", 15.5, 30))
+    
     db.session.add(Employment(300, 100, datetime(2021, 1, 1), datetime(2021, 12, 1)))
     db.session.add(Employment(301, 101, datetime(2021, 3, 6), datetime(2021, 11, 7)))
     db.session.add(Employment(302, 102, datetime(2021, 5, 10), datetime(2021, 9, 11)))
+    
     db.session.commit()
