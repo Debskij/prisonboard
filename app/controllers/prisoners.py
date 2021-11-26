19aee7b78ea8ecece7c090f251d182a1357ccd05
BazyliP <bazyli.polednia@gmail.com>
2021-11-26 16:01:19 +0100

    WIP list companies for prisoners

--- app/controllers/prisoners.py
+++ app/controllers/prisoners.py
@@ -4,14 +4,29 @@ from app import app, db
 from app.models import Prisoner, Employment, JobOffer, Company
 from sqlalchemy import func
 from flask import render_template, redirect, url_for, abort, request
+from dataclasses import dataclass
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 
+@dataclass
+class PrisonerQueryResult:
+    prisoner: Prisoner
+    company: Company
+    
+def get_prisoners_with_companies(prisoners_query_results):
+    prisoners = []
+    for prisoner in prisoners_query_results:
+        companies_query = db.session.query(Company)
+        company = companies_query.join(Company, JobOffer, Employment).join(JobOffer.related_employment).filter(Employment.employee_id == prisoner.id)
+        prisoners.append(PrisonerQueryResult(prisoner, company))
+    return prisoners
+
 @app.route("/prisoners", methods=["GET"], strict_slashes=False)
 def get_all_prisoners():
-    prisoners = Prisoner.query.all()
+    prisoners_query_results = Prisoner.query.all()
+    prisoners = get_prisoners_with_companies(prisoners_query_results)
     return render_template("prisoners.html", prisoners=prisoners)
 
 
@@ -28,21 +43,24 @@ def get_prisoners_by_field_redirect():
 @app.route("/prisoners/name/<value>", methods=["GET"])
 def get_prisoners_by_name(value):
     query = db.session.query(Prisoner)
-    prisoners = query.filter(func.lower(Prisoner.name) == value.lower())
+    prisoners_query_results = query.filter(func.lower(Prisoner.name) == value.lower())
+    prisoners = get_prisoners_with_companies(prisoners_query_results)
     return render_template("prisoners.html", prisoners=prisoners)
 
 
 @app.route("/prisoners/surname/<value>", methods=["GET"])
 def get_prisoners_by_surname(value):
     query = db.session.query(Prisoner)
-    prisoners = query.filter(func.lower(Prisoner.surname) == value.lower())
+    prisoners_query_results = query.filter(func.lower(Prisoner.surname) == value.lower())
+    prisoners = get_prisoners_with_companies(prisoners_query_results)
     return render_template("prisoners.html", prisoners=prisoners)
 
 
 @app.route("/prisoners/company/<value>", methods=["GET"])
 def get_prisoners_by_company(value):
     query = db.session.query(Prisoner).join(Employment).join(JobOffer).join(Company)
-    prisoners = query.filter(
+    prisoners_query_results = query.filter(
         func.lower(Company.full_name).contains(value.lower())
     ).all()
+    prisoners = get_prisoners_with_companies(prisoners_query_results)
     return render_template("prisoners.html", prisoners=prisoners)
